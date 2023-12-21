44977838d4d79c7901a950596f56890d4fcd4b8b
Sudhanshu-21 <77183130+Sudhanshu-21@users.noreply.github.com>
2023-12-21 20:58:14 +0530

    First commit after correcting responsiveness of event carousel

--- app/eventCard.js
+++ app/eventCard.js
@@ -1,26 +1,72 @@
 import React from "react";
-import { Button, Card } from "flowbite-react";
-function EventCard({ id, img, name, content }) {
+import { Button } from "flowbite-react";
+function EventCard({ img, name, content, width, height }) {
+  var wn = "text-2xl";
+  var wc = "text-base";
+  if (width <= 0.8 * height) {
+    if (width >= 1800) {
+      wn = "text-6xl";
+      wc = "text-4xl";
+    } else if (width >= 1536) {
+      wn = "text-5xl";
+      wc = "text-3xl";
+    } else if (width >= 1280) {
+      wn = "text-4xl";
+      wc = "text-2xl";
+    } else if (width >= 1024) {
+      wn = "text-5xl";
+      wc = "text-3xl";
+    } else if (width >= 768) {
+      wn = "text-3xl";
+      wc = "text-xl";
+    }
+  } else if (width <= 1.4 * height) {
+    if (width >= 1800) {
+      wn = "text-6xl";
+      wc = "text-4xl";
+    } else if (width >= 1536) {
+      wn = "text-5xl";
+      wc = "text-3xl";
+    } else if (width >= 1024) {
+      wn = "text-4xl";
+      wc = "text-2xl";
+    } else if (width >= 768) {
+      wn = "text-3xl";
+      wc = "text-xl";
+    }
+  } else {
+    if (width >= 1800) {
+      wn = "text-5xl";
+      wc = "text-3xl";
+    } else if (width >= 1280) {
+      wn = "text-4xl";
+      wc = "text-xl";
+    } else if (width >= 1024) {
+      wc = "text-xl";
+    }
+  }
   return (
     <div className="bg-[#E8D9C6] h-full border-0 border-[#655643] grid grid-row-3 sm:w-full drop-shadow-2xl rounded-xl overflow-hidden">
       <div className="w-full overflow-y-auto ">
         <img className="w-full " src={img}></img>
       </div>
-      <div className=" pt-2 px-4 text-center overflow-auto">
-        <h5 className="py-1 md:py-3 text-2xl lg:text-3xl 2xl:text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <div className="pt-2 px-4 text-center overflow-auto">
+        <h5
+          className={`py-1 md:py-3 ${wn} font-bold tracking-tight text-gray-900 dark:text-white`}
+        >
           {name}
         </h5>
-        <p className="font-normal lg:text-xl 2xl:text-xl text-gray-700 dark:text-gray-400">
+        <p className={`font-normal ${wc} text-gray-700 dark:text-gray-400`}>
           {content}
         </p>
       </div>
-      <div className="pt-2 px-4 text-center text-5xl flex flex-col justify-center mb-10">
-        <Button
+      <div className="pt-2 px-4 text-center text-2xl font-bold flex flex-col justify-center mx-2 mb-10">
+        <button
           size="xl"
-          className="bg-[#B29B7E] border-2 border-[#655643] hover:scale-105"
+          className="py-2 bg-[#B29B7E] border-2 border-[#655643] hover:bg-[#876f52] hover:scale-105 hover:border-[3px] hover:shadow-lg rounded-md"
         >
           Register Now
-        </Button>
+        </button>
       </div>
     </div>
   );
--- app/eventsCarousel.js
+++ app/eventsCarousel.js
@@ -1,9 +1,36 @@
 "use client";
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { Carousel, Card } from "flowbite-react";
 import EventCard from "./eventCard";
 function EventsCarousel() {
-  // Array for Event Details 
+  // Function to get height and width of screen
+  const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
+  const updateScreenSize = () => {
+    setScreenSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+  useEffect(() => {
+    updateScreenSize();
+    window.addEventListener("resize", updateScreenSize);
+    return () => {
+      window.removeEventListener("resize", updateScreenSize);
+    };
+  }, []);
+  // Customising height of carousel based on screen ration
+  var h = "h-[60vh]";
+
+  if (screenSize.height > screenSize.width) {
+    if (screenSize.height > 2 * screenSize.width) {
+      h = "h-[40vh]";
+    } else {
+      h = "h-[50vh]";
+    }
+  } else {
+    h = "h-[80vh]";
+  }
+  // Array for Event Details
   const event = [
     {
       id: 1,
@@ -21,7 +48,7 @@ function EventsCarousel() {
     },
     {
       id: 3,
-      img: "/vercel.svg",
+      img: "/img3.jpg",
       name: "Vaad-V-Vaad",
       content:
         "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order. Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
@@ -42,7 +69,7 @@ function EventsCarousel() {
     },
     {
       id: 6,
-      img: "/vercel.svg",
+      img: "/img3.jpg",
       name: "Pen Flow",
       content:
         "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order. Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
@@ -70,7 +97,7 @@ function EventsCarousel() {
     },
     {
       id: 10,
-      img: "/vercel.svg",
+      img: "/img3.jpg",
       name: "Surmay Sandhya",
       content:
         "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order. Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
@@ -90,14 +117,14 @@ function EventsCarousel() {
           Events
         </div>
         <div className="mx-[10%] sm:mx-[20%] lg:mx-[10%] ">
-          <div className="h-[60vh] sm:h-[80vh] flex justify-center">
+          <div className={`${h} flex justify-center`}>
             <Carousel
               slideInterval={5000}
               pauseOnHover
               className="w-[80vw] sm:w-[70vw] md:w-[65vw] lg:w-[60vw] xl:w-[55vw] 2xl:w-[45vw]"
             >
               {event.map(({ id, img, name, content }) => (
-                <EventCard key={id} img={img} name={name} content={content} />
+                <EventCard key={id} img={img} name={name} content={content} width={screenSize.width} height={screenSize.height} />
               ))}
             </Carousel>
           </div>
